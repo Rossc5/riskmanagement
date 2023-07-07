d5d5287f7133d676c8ac82a4972589836a7143b7
cld200-a06 SAP Education <cld200-a06@education.cloud.sap>
2023-07-07 14:22:08 +0000

    first commit

---
+++ app/risks/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'riks/test/integration/FirstJourney',
+		'riks/test/integration/pages/RisksList',
+		'riks/test/integration/pages/RisksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, RisksList, RisksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('riks') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheRisksList: RisksList,
+					onTheRisksObjectPage: RisksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
